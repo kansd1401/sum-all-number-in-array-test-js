642a69728ec51d254626148f44bc94b6a6d5825c
kansd1401 <kang.sanjeet1401@gmail.com>
2019-10-18 21:20:23 +0000

    added recurusive array func

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  let sum =0;
+  for(num of array){
+    if(Array.isArray(num)){
+        sum+=sumItems(num)
+    }
+    else{
+      sum+=num 
+    }
+  }
+  return sum
 }
 
 module.exports = sumItems;
